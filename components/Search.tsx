657563ec828b5b4c7f5faa7d52516d809e535752
BisiDavi <oludavidconnect@gmail.com>
2023-01-20 00:35:59 +0100

    search updated

--- components/Search.tsx
+++ components/Search.tsx
@@ -5,11 +5,11 @@ export default function Search() {
     <form className="container mx-auto relative -mt-14 rounded-full">
       <input
         placeholder="Enter Product Name"
-        className="placeholder:text-gray-400 text-2xl py-8 px-10 placeholder:text-2xl w-full border rounded-full shadow"
+        className="placeholder:text-gray-400 text-2xl py-6 px-10 placeholder:text-2xl w-full border rounded-full shadow"
       />
       <Button
         text="Search"
-        className="px-8 py-4 w-40 text-xl bg-lightOrange text-white rounded-full absolute right-8 top-5"
+        className="px-8 py-3 w-40 text-xl bg-lightOrange text-white rounded-full absolute right-8 top-4"
       />
     </form>
   );

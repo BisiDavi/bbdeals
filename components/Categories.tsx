5825042d9e327c3146f8bdbb635a43bcb16be44b
BisiDavi <oludavidconnect@gmail.com>
2023-01-19 18:33:47 +0100

    product updated

--- components/Categories.tsx
+++ components/Categories.tsx
@@ -1,3 +1,16 @@
+import categoryContent from "@/json/products.json";
+
 export default function Categories() {
-  return <aside>Categories</aside>;
+  return (
+    <aside className="w-1/4">
+      <h2 className="text-xl my-2">Categories</h2>
+      <ul className="shadow py-4 px-6 rounded-xl my-4">
+        {categoryContent.categories.map((item) => (
+          <li key={item.category} className="my-4 border-b last:border-b-0">
+            {item.category}
+          </li>
+        ))}
+      </ul>
+    </aside>
+  );
 }
--- components/ProductView.tsx
+++ components/ProductView.tsx
@@ -1,5 +1,5 @@
 import type { PropsWithChildren } from "react";
 
 export default function ProductView({ children }: PropsWithChildren) {
-  return <div className="container py-8">{children}</div>;
+  return <div className="container py-8 mx-auto">{children}</div>;
 }

60193163a702bf8b375fc8302f7fc02682d4c51e
BisiDavi <oludavidconnect@gmail.com>
2023-01-20 01:22:16 +0100

    product updated

--- components/ProductGrid.tsx
+++ components/ProductGrid.tsx
@@ -1,8 +1,8 @@
+import { useState } from "react";
+import Image from "next/image";
 import Button from "@/components/Button";
 import Product from "@/components/Product";
 import productsContent from "@/json/products.json";
-import Image from "next/image";
-import { useState } from "react";
 
 export default function ProductGrid() {
   const [productCategory, setProductCategory] = useState("Trending");
@@ -11,14 +11,12 @@ export default function ProductGrid() {
     setProductCategory(item);
   }
 
-  const categories = ["Trending", "Near to Spot", "Newly Launched"];
-
   return (
     <div>
       <h2 className="font-normal text-3xl">Top Products</h2>
       <div className="group flex mt-4 justify-between items-center">
         <div className="left space-x-2">
-          {categories.map((item) => {
+          {productsContent.productCategories.map((item) => {
             const style =
               productCategory === item
                 ? "bg-lightOrange text-white"
@@ -33,16 +31,24 @@ export default function ProductGrid() {
             );
           })}
         </div>
-        <ul className="right flex space-x-4">
-          <li className="flex items-center space-x-2">
-            <Image src="/list.png" height={30} width={30} alt="list" />
-            <p>Detailed View</p>
-          </li>
-          <li className="flex items-center space-x-2">
-            <Image src="/list.png" height={30} width={30} alt="list" />
-            <p>Grid View</p>
-          </li>
-        </ul>
+        <div className="right flex space-x-4">
+          {productsContent.views.map((item) => (
+            <Button
+              key={item.text}
+              icon={
+                <Image
+                  src={item.icon}
+                  height={30}
+                  width={30}
+                  alt={item.text}
+                  className="mr-1"
+                />
+              }
+              text={item.text}
+              className="flex items-center hover:bg-gray-100 px-4 hover:rounded-full py-2"
+            />
+          ))}
+        </div>
       </div>
       <div className="grid grid-cols-4 gap-6 mt-4">
         {productsContent.products.map((product, index) => (

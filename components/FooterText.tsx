7c3155b37bdb65659daf422d017a64d09b3d86e5
BisiDavi <oludavidconnect@gmail.com>
2023-01-20 01:38:47 +0100

    footer text added

---
+++ components/FooterText.tsx
@@ -0,0 +1,53 @@
+export default function FooterText() {
+  return (
+    <div className="container mx-auto my-8 mb-14">
+      <h2 className="text-xl">Heading here</h2>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis eaque
+      impedit laudantium perferendis, sequi nesciunt soluta adipisci rem
+      ratione. Alias voluptatum repudiandae accusantium error? Reiciendis ut
+      autem quos expedita eveniet? Lorem ipsum dolor sit amet consectetur
+      adipisicing elit. Debitis eaque impedit laudantium perferendis, sequi
+      nesciunt soluta adipisci rem ratione. Alias voluptatum repudiandae
+      accusantium error? Reiciendis ut autem quos expedita eveniet?
+      <br />
+      <br />
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis eaque
+      impedit laudantium perferendis, sequi nesciunt soluta adipisci rem
+      ratione. Alias voluptatum repudiandae accusantium error? Reiciendis ut
+      autem quos expedita eveniet? Lorem ipsum dolor sit amet consectetur
+      adipisicing elit. Debitis eaque impedit laudantium perferendis, sequi
+      nesciunt soluta adipisci rem ratione. Alias voluptatum repudiandae
+      accusantium error? Reiciendis ut autem quos expedita eveniet? Lorem ipsum
+      dolor sit amet consectetur adipisicing elit. Debitis eaque impedit
+      laudantium perferendis, sequi nesciunt soluta adipisci rem ratione. Alias
+      voluptatum repudiandae accusantium error? Reiciendis ut autem quos
+      expedita eveniet?
+      <br />
+      <br />
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis eaque
+      impedit laudantium perferendis, sequi nesciunt soluta adipisci rem
+      ratione. Alias voluptatum repudiandae accusantium error? Reiciendis ut
+      autem quos expedita eveniet? Lorem ipsum dolor sit amet consectetur
+      adipisicing elit. Debitis eaque impedit laudantium perferendis, sequi
+      nesciunt soluta adipisci rem ratione. Alias voluptatum repudiandae
+      accusantium error? Reiciendis ut autem quos expedita eveniet? Lorem ipsum
+      dolor sit amet consectetur adipisicing elit. Debitis eaque impedit
+      laudantium perferendis, sequi nesciunt soluta adipisci rem ratione. Alias
+      voluptatum repudiandae accusantium error? Reiciendis ut autem quos
+      expedita eveniet?
+      <br />
+      <br />
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis eaque
+      impedit laudantium perferendis, sequi nesciunt soluta adipisci rem
+      ratione. Alias voluptatum repudiandae accusantium error? Reiciendis ut
+      autem quos expedita eveniet? Lorem ipsum dolor sit amet consectetur
+      adipisicing elit. Debitis eaque impedit laudantium perferendis, sequi
+      nesciunt soluta adipisci rem ratione. Alias voluptatum repudiandae
+      accusantium error? Reiciendis ut autem quos expedita eveniet? Lorem ipsum
+      dolor sit amet consectetur adipisicing elit. Debitis eaque impedit
+      laudantium perferendis, sequi nesciunt soluta adipisci rem ratione. Alias
+      voluptatum repudiandae accusantium error? Reiciendis ut autem quos
+      expedita eveniet?
+    </div>
+  );
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,24 +2,23 @@ import AdBanner from "@/components/AdBanner";
 import AllFavourites from "@/components/AllFavourites";
 import Banner from "@/components/Banner";
 import Categories from "@/components/Categories";
+import FooterText from "@/components/FooterText";
 import ProductGrid from "@/components/ProductGrid";
 import ProductView from "@/components/ProductView";
-import Search from "@/components/Search";
 import Layout from "@/layout";
 
 export default function Home() {
   return (
     <Layout title="Welcome, find bullions at best prices">
-      <section>
-        <Banner />
-        <AdBanner url="/ad-banner-1.webp" />
-        <ProductView>
-          <Categories />
-          <ProductGrid />
-        </ProductView>
-        <AdBanner url="/ad-banner-2.webp" className="container mx-auto" />
-        <AllFavourites />
-      </section>
+      <Banner />
+      <AdBanner url="/ad-banner-1.webp" />
+      <ProductView>
+        <Categories />
+        <ProductGrid />
+      </ProductView>
+      <AdBanner url="/ad-banner-2.webp" className="container mx-auto" />
+      <AllFavourites />
+      <FooterText />
     </Layout>
   );
 }
